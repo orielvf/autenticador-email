8150da0110e4a755982465d2c64f13f39377a6e8
orielvf <orielvf@gmail.com>
2025-05-15 23:35:11 -0300

    Cadastrar campanha

---
+++ routes/cadastrarCampanha.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+router.post("/", async (req, res) => {
+    try {
+        const { nome, descricao, data_inicio, data_termino, quantidadeAleatoria } = req.body;
+
+        const novaCampanha = await prisma.campanha.create({
+            data: {
+                nome,
+                descricao,
+                data_inicio: new Date(data_inicio),
+                data_termino: new Date(data_termino),
+                quantidadeAleatoria,
+                usuari_id,
+            },
+        });
+
+        res.status(201).json(novaCampanha);
+    } catch (error) {
+        console.error("Erro ao cadastrar campanha:", error);
+        res.status(500).json({ error: "Erro ao cadastrar campanha" });
+    }
+});
+
+module.exports = router;
