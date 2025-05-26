c5ab7841778bd3ede91d7453253c65b0e5179378
orielvf <orielvf@gmail.com>
2025-05-26 19:45:43 -0300

    Refactor: Adicionar campos webhook na resposta do login

--- routes/login.js
+++ routes/login.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const pool = require('../db'); // Conexão com o banco de dados
+const pool = require('../db');
 const router = express.Router();
 
-// Rota de login
 router.post('/', async (req, res) => {
     console.log('Requisição de login recebida:', req.body);
     const { email, senha } = req.body;
 
     try {
-        // Consultar o banco para encontrar o usuário
         const query = 'SELECT * FROM usuarios WHERE email = $1';
         const { rows } = await pool.query(query, [email]);
 
-        // Verificar se o usuário foi encontrado
         if (rows.length === 0) {
             return res.status(401).json({
                 sucesso: false,
@@ -22,12 +19,6 @@ router.post('/', async (req, res) => {
         }
 
         const user = rows[0];
-
-        // Verificar se a senha fornecida corresponde à senha_hash no banco
-        //console.log('Senha digitada:', senha);
-        //console.log('Hash no banco:', user.senha_hash);
-
-
         const isMatch = await bcrypt.compare(senha, user.senha_hash);
 
         if (!isMatch) {
@@ -37,11 +28,16 @@ router.post('/', async (req, res) => {
             });
         }
 
-        // Se tudo der certo, retorna a resposta de sucesso
         return res.json({
             sucesso: true,
             mensagem: 'Login bem-sucedido',
-            user: { email: user.email, id: user.id },
+            user: {
+                email: user.email,
+                id: user.id,
+                npswebhook: user.npswebhook,
+                indicadorwebhook: user.indicadorwebhook,
+                promotorwebhook: user.promotorwebhook,
+            },
         });
 
     } catch (err) {
